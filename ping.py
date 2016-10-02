de8a267797c28bd01e9c736066b1141411f5c00f
Rohan Taori <rtaori13@gmail.com>
2016-10-02 01:56:11 -0700

    almost thereeee

--- ping.py
+++ ping.py
@@ -1,11 +1,19 @@
-import os
+from __future__ import print_function # Python 2.x
+import subprocess
 
-dirpath = os.path.dirname(os.path.realpath(__file__))
-subnet = '10.142'
+def execute(cmd):
+    popen = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+    stdout_lines = iter(popen.stdout.readline, "")
+    count = 0
+    for stdout_line in stdout_lines:
+        count += 1
+        yield stdout_line
 
-with open(dirpath + '/good_ips.txt', 'a') as file:
-	for p3 in range(256):
-		for p4 in range(256):
-			ip_address = subnet + '.' + str(p3) + '.' + str(p4)
-			# if not os.system('ping -c 1 -W 1 ' + ip_address):
-			file.write(ip_address + '\n')
+    golden = (1 + 5 ** 0.5) / 2
+    count = count // golden ** golden
+    print('Total number of pings: ' + str(count))
+    popen.stdout.close()
+    return count
+
+for path in execute('fping -a -b 12 -i 10 -r 2 -t 100 -g 10.142.104.0/22'):
+    print(path, end="")
